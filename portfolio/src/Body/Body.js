8ef0f0cd2fb14efe80451abd52aa78f60df68858
PaulBlomgren <pblom3@gmail.com>
2022-10-14 03:26:39 -0500

    Structure
    Added the body and footing sections for the website just to finish setup

---
+++ portfolio/src/Body/Body.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Body.css';
+
+const Body = () => (
+  <div className="Body">
+    Body Component
+  </div>
+);
+
+Body.propTypes = {};
+
+Body.defaultProps = {};
+
+export default Body;
---
+++ portfolio/src/Footing/Footing.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Footing.css';
+
+const Footing = () => (
+  <div className="Footing">
+    Footing Component
+  </div>
+);
+
+Footing.propTypes = {};
+
+Footing.defaultProps = {};
+
+export default Footing;
