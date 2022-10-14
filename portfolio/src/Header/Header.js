d5b830d67326014b111469d20d57febf55ee1ac7
PaulBlomgren <pblom3@gmail.com>
2022-10-14 01:15:38 -0500

    Created react app
    Made a header

---
+++ portfolio/src/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Header.css';
+
+const Header = () => (
+  <div class="topnav">
+    <a class="active" href="#home">Home</a>
+    <a href="#news">News</a>
+    <a href="#contact">Contact</a>
+    <a href="#about">About</a>
+  </div>
+);
+
+
+Header.propTypes = {};
+
+Header.defaultProps = {
+};
+
+export default Header;
